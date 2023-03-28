d523f5f4b43090c4c0758b3ba7dd10bfe28f9100
VivienIngrams <vivingrams@gmail.com>
2023-03-28 15:17:57 +0100

    Data filled in

---
+++ src/data.js
@@ -0,0 +1,50 @@
+let data = {
+    "name": "Celeste Ingrams",
+    "role": "Artist",
+    "linkedinId":"linkedIn Id",
+    "facebook": "facebookId",
+    "roleDescription": "I do art for communities and wellbeing",
+    "socialLinks":[
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com",
+          "className":"fa fa-facebook"
+        },
+        {
+          "name":"instagram",
+          "url":"http://instagram.com",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"This is my final project for my Masters in Art, Health and Wellbeing at the University of South Wales",
+    "address":"Southampton",
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/celeste.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/celeste1.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/celeste2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/celeste3.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/remoteWork.jpg"
+      }
+    ],
+  }
+  
+  export default data
