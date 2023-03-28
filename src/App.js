c854c376e5adec3a545520551a52caea798d42bc
VivienIngrams <vivingrams@gmail.com>
2023-03-28 15:25:32 +0100

    ABout page working

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 
 import Header from './components/Header';
 import About from './components/About';
-import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
-import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import data from './data';
 
@@ -13,12 +11,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header data={data}/>
+        {/* <Header data={data}/> */}
         <About data={data}/>
-        <Resume data={data}/>
-        <Portfolio data={data}/>
-        <ContactUs data={data}/>
-        <Footer data={data}/>
+        {/* <Portfolio data={data}/> */}
+        {/* <Footer data={data}/> */}
       </div>
     );
   }
---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    let data = this.props.data;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="profile-pic"  src="images/celesteFace.jpg" alt="" />
+
+            </div>
+
+            <div className="nine columns main-col">
+
+               <h2>About Me</h2>
+               <p>
+               {
+                 data.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>{data.name}</span>
+                     <br></br>
+       						   <span>
+                     {data.address}
+                    </span>
+                   
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
+}
